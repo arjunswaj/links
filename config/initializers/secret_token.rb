3dcbad063ed0aaf3799b3180108aab9c48484ca7
Abhijith Madhav <browseanddownload@gmail.com>
2014-03-21 05:49:07 +0530

    Reverting

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '0f00af89b6e323e6926baa34efe597c19ac88c8f2e75798810c72c70fd841f622cecfbb5db26c1a513dfd73988300f60e06efc07be763b946c76b9a8b5661e1b'
