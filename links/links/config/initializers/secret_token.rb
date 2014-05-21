a049366de6723c3dc10f10d4cdf805da9e2c119b
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2014-05-21 10:31:34 +0530

    Creating a rails project for links. From here branching can be done for creation of features and merging will be done once the feature is completed.

---
+++ links/links/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Links::Application.config.secret_key_base = 'c4e316603958de0820400654c20d035c5f1556e0f12eac46b544026d5975691e46746d5fcbd7271fff1345d629a5eeff5450acdbad1bff214b0192916d5b2b51'
