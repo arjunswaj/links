b89fedbc1046c85b9cc8acce381c6f3edabd4548
Abhijith Madhav <browseanddownload@gmail.com>
2014-05-30 23:14:51 +0530

    Access to timeline page is now gaurded through sign_in. Have now reverted previous checkin as is no longer needed

--- links/links/app/controllers/bookmarks_controller.rb
+++ links/links/app/controllers/bookmarks_controller.rb
@@ -1,5 +1,5 @@
 class BookmarksController < ApplicationController
-  before_action  :authenticate_user!, only: [:show, :edit, :update, :destroy, :index] #TODO: Can I have a set_bookmark here??
+  before_action  :authenticate_user!, only: [:show, :edit, :update, :destroy, :index, :timeline] #TODO: Can I have a set_bookmark here??
 
   layout "links"
 
