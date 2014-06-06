cdad63719d48176f9c87e56a9b2203331433c6db
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2014-06-06 23:22:04 +0530

    Using bootstrap for Add and Edit Bookmark forms.

--- links/links/app/assets/javascripts/bookmark_form.js
+++ links/links/app/assets/javascripts/bookmark_form.js
@@ -1,9 +1,18 @@
 var ms_tags;
 function initMagicSuggest(prepopulatedValues) {
   ms_tags = $('#ms_tags').magicSuggest({
-    	minChars: 2,
-      data: [],
-		  value: prepopulatedValues,  		
-  		highlight: false       
+   	minChars: 2,
+   	style: 'padding: 0px',
+    data: [],
+	value: prepopulatedValues,  		
+  	highlight: false       
   });
 }
+
+$(function() {
+    $(document).on('click', '#cancel_bookmark', function(event) {
+        $("#overlay").remove();
+		$('.timeline').prepend('<div id="overlay" class="overlay-invisible"></div>');
+		$("#url").val("");
+    })
+});
