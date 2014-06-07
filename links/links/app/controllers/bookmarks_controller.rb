abde359598ace53f402bd3f5d6857d1614f2fe8f
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2014-06-07 23:59:15 +0530

    Correction in Timeline Query.

--- links/links/app/controllers/bookmarks_controller.rb
+++ links/links/app/controllers/bookmarks_controller.rb
@@ -131,8 +131,9 @@ class BookmarksController < ApplicationController
   # GET /bookmarks.json
   def index
     @bookmark_plugins = PLUGIN_CONFIG['bookmark']
-    @bookmarks = Bookmark.eager_load(:tags, :user, :url, :group)
-      .where("(users.id = :user_id AND bookmarks.group_id IS NULL) OR bookmarks.group_id IS NOT NULL", user_id: "#{current_user.id}")    
+    @bookmarks = Bookmark.eager_load(:tags, :user, :url)
+      .eager_load(group: :memberships)
+      .where("(users.id = :user_id AND bookmarks.group_id IS NULL) OR (bookmarks.group_id IS NOT NULL AND memberships.user_id = :user_id AND memberships.acceptance_status = :membership_status)", user_id: "#{current_user.id}", membership_status: "t")    
       .order('bookmarks.updated_at DESC')
   end
 
--- links/links/app/controllers/searches_controller.rb
+++ links/links/app/controllers/searches_controller.rb
@@ -16,13 +16,16 @@ class SearchesController < ApplicationController
     @bookmark_plugins = PLUGIN_CONFIG['bookmark']
     if keyword.start_with?('#')
       tagname = keyword[1, keyword.length].strip.gsub(' ', '-').downcase
-      @bookmarks = Bookmark.eager_load(:tags, :user, :url).where("LOWER(tags.tagname) = LOWER(:tag)", tag: "#{tagname}")
-      .where("users.id = :user_id", user_id: "#{current_user.id}")
+      @bookmarks = Bookmark.eager_load(:tags, :user, :url)
+      .eager_load(group: :memberships)
+      .where("(users.id = :user_id AND bookmarks.group_id IS NULL) OR (bookmarks.group_id IS NOT NULL AND memberships.user_id = :user_id AND memberships.acceptance_status = :membership_status)", user_id: "#{current_user.id}", membership_status: "t")    
+      .where("LOWER(tags.tagname) = LOWER(:tag)", tag: "#{tagname}")
       .order('bookmarks.updated_at DESC')      
     else
-      @bookmarks = Bookmark.eager_load(:tags, :user, :url).where("LOWER(title) LIKE LOWER(:query) OR LOWER(description) LIKE LOWER(:query)", 
-      query: "%#{keyword}%")
-      .where("users.id = :user_id", user_id: "#{current_user.id}")
+      @bookmarks = Bookmark.eager_load(:tags, :user, :url)
+      .eager_load(group: :memberships)
+      .where("(users.id = :user_id AND bookmarks.group_id IS NULL) OR (bookmarks.group_id IS NOT NULL AND memberships.user_id = :user_id AND memberships.acceptance_status = :membership_status)", user_id: "#{current_user.id}", membership_status: "t")
+      .where("LOWER(title) LIKE LOWER(:query) OR LOWER(description) LIKE LOWER(:query)", query: "%#{keyword}%")      
       .order('bookmarks.updated_at DESC') 
     end            
 
