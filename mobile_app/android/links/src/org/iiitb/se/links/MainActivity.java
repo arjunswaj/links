dde6286c9b2b9e6bc5027fdd9e7da9440df6a94d
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2014-06-17 22:53:25 +0530

    Moving out LinkFragment to a separate file.

--- mobile_app/android/links/src/org/iiitb/se/links/MainActivity.java
+++ mobile_app/android/links/src/org/iiitb/se/links/MainActivity.java
@@ -1,6 +1,7 @@
 package org.iiitb.se.links;
 
 
+import org.iiitb.se.links.home.fragments.LinkFragment;
 import org.iiitb.se.links.utils.AppConstants;
 import org.iiitb.se.links.utils.URLConstants;
 import org.scribe.builder.ServiceBuilder;
@@ -49,8 +50,7 @@ public class MainActivity extends Activity {
   private CharSequence mDrawerTitle;
   private CharSequence mTitle;
   private String[] mLinksOptions;
-
-  private static final Token EMPTY_TOKEN = null;
+    
   private static final String TAG = "MainActivity";
 
   @Override
@@ -191,144 +191,5 @@ public class MainActivity extends Activity {
     super.onConfigurationChanged(newConfig);
     // Pass any configuration change to the drawer toggls
     mDrawerToggle.onConfigurationChanged(newConfig);
-  }
-
-  /**
-   * Fragment that appears in the "content_frame", shows Links
-   */
-  public static class LinkFragment extends Fragment {
-    public static final String LINK_OPTION_NUMBER = "link_option_number";
-    private WebView mWebView;
-    private TextView tv;
-    private Dialog auth_dialog;
-    private OAuthService mOauthService;
-    private Token mRequestToken;
-
-    public LinkFragment() {
-      // Empty constructor required for fragment subclasses
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-        Bundle savedInstanceState) {
-      View rootView = inflater.inflate(R.layout.fragment_links, container,
-          false);
-      auth_dialog = new Dialog(getActivity());
-      auth_dialog.setContentView(R.layout.auth_dialog);      
-      mWebView = (WebView) auth_dialog.findViewById(R.id.webView);
-      mWebView.clearCache(true);
-      mWebView.getSettings().setJavaScriptEnabled(true);
-      mWebView.setWebViewClient(mWebViewClient);
-      
-      int i = getArguments().getInt(LINK_OPTION_NUMBER);
-      String linkOption = getResources().getStringArray(R.array.links_options)[i];
-      getActivity().setTitle(linkOption);
-
-      tv = (TextView) rootView.findViewById(R.id.result);
-      mOauthService = new ServiceBuilder().provider(LinksApi.class)
-          .apiKey(AppConstants.API_KEY).apiSecret(AppConstants.API_SECRET)
-          .callback(AppConstants.URN_IETF_WG_OAUTH_2_0_OOB).build();
-
-      SharedPreferences sharedPreferences = getActivity().getPreferences(
-          MODE_PRIVATE);
-      String accessTokenKey = sharedPreferences.getString(
-          AppConstants.ACCESS_TOKEN_KEY, null);
-      String accessTokenSecret = sharedPreferences.getString(
-          AppConstants.ACCESS_TOKEN_SECRET, null);
-      if (null == accessTokenKey || null == accessTokenSecret) {
-        Log.i(TAG, "Token Key is not saved. Will start authorization.");              
-        auth_dialog.show();
-        auth_dialog.setTitle("Authorize Links");
-        auth_dialog.setCancelable(true);
-        startAuthorize();
-      } else {
-        Log.i(TAG,
-            "Token Key found. Will access protected resource - bookmarks.");
-        Token accessToken = new Token(accessTokenKey, accessTokenSecret);
-        fetchBookmarks(accessToken);
-      }
-      return rootView;
-    }
-
-    private void fetchBookmarks(final Token accessToken) {      
-      (new AsyncTask<Void, Void, String>() {
-        Response response;
-        int status;
-        @Override
-        protected String doInBackground(Void... params) {
-          OAuthRequest request = new OAuthRequest(Verb.GET,
-              URLConstants.PROTECTED_RESOURCE_URL_BOOKMARKS);
-          mOauthService.signRequest(accessToken, request);
-          response = request.send();
-          status = response.getCode();
-          return response.getBody();
-        }
-
-        @Override
-        protected void onPostExecute(String responseBody) {
-          if(null == responseBody || 401 == status) {
-            startAuthorize();
-          } else {
-            tv.setText(responseBody);
-          }
-        }
-
-      }).execute();
-
-    }
-
-    private void startAuthorize() {
-      (new AsyncTask<Void, Void, String>() {
-        @Override
-        protected String doInBackground(Void... params) {
-          return mOauthService.getAuthorizationUrl(EMPTY_TOKEN);
-        }
-
-        @Override
-        protected void onPostExecute(String url) {
-          mWebView.loadUrl(url);
-        }
-      }).execute();
-    }
-
-    private WebViewClient mWebViewClient = new WebViewClient() {
-         
-      @Override
-      public void onPageStarted(WebView view, String url, Bitmap favicon) {
-        if ((url != null) && (url.startsWith(URLConstants.CALLBACK_URL))) {
-          // Override webview when user came back to CALLBACK_URL
-          mWebView.stopLoading();
-          mWebView.setVisibility(View.INVISIBLE); // Hide webview if necessary
-          String authorizationCode = url.substring(URLConstants.CALLBACK_URL
-              .length());
-          final Verifier verifier = new Verifier(authorizationCode);
-          (new AsyncTask<Void, Void, Token>() {
-            @Override
-            protected Token doInBackground(Void... params) {
-              Token token = mOauthService.getAccessToken(mRequestToken,
-                  verifier);
-              SharedPreferences.Editor editor = getActivity().getPreferences(
-                  MODE_PRIVATE).edit();
-              editor.putString(AppConstants.ACCESS_TOKEN_KEY, token.getToken());
-              editor.putString(AppConstants.ACCESS_TOKEN_SECRET,
-                  token.getSecret());
-              editor.commit();
-              Log.i(TAG, "Saving the token key.");
-              return token;
-            }
-
-            @Override
-            protected void onPostExecute(final Token accessToken) {
-              auth_dialog.dismiss();
-              fetchBookmarks(accessToken);
-            }
-          }).execute();
-        } else {
-          super.onPageStarted(view, url, favicon);
-        }
-      }
-    };
-
-  }
-
+  }  
 }
