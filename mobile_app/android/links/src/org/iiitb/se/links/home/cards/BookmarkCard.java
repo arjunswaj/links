7f3dcea09781df474618cea10be004f29d8588b2
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2014-06-22 18:21:17 +0530

    Custom card UI with domain extractor and Web page launching intent. Refactoring of code.

---
+++ mobile_app/android/links/src/org/iiitb/se/links/home/cards/BookmarkCard.java
@@ -0,0 +1,148 @@
+package org.iiitb.se.links.home.cards;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.iiitb.se.links.R;
+import org.iiitb.se.links.utils.DomainExtractor;
+import org.iiitb.se.links.utils.StringConstants;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardExpand;
+import it.gmariotti.cardslib.library.internal.CardHeader;
+import it.gmariotti.cardslib.library.internal.ViewToClickToExpand;
+import it.gmariotti.cardslib.library.internal.base.BaseCard;
+import it.gmariotti.cardslib.library.view.CardView;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class BookmarkCard extends Card {
+
+	protected TextView mDomain;
+	protected TextView mUpdatedTime;
+	protected TextView mMore;
+	JSONObject bookmark;
+	Context context;
+	String id = null;
+	String url = null;
+	String title = null;
+	String description = null;
+	String formattedDate = null;
+	CardView cardView = null;
+	CardHeader header = null;
+	CardExpand cardExpand = null;
+
+	public JSONObject getBookmark() {
+		return bookmark;
+	}
+
+	public void setBookmark(JSONObject bookmark) {
+		this.bookmark = bookmark;
+		initData();
+		setData();
+	}
+
+	/**
+	 * Constructor with a custom inner layout
+	 * 
+	 * @param context
+	 */
+	public BookmarkCard(Context context, JSONObject bookmark) {
+		super(context, R.layout.bookmark_card);
+		this.bookmark = bookmark;
+		this.context = context;
+		init();
+	}
+
+	/**
+	 * Init
+	 */
+	private void init() {
+
+		header = new CardHeader(context);
+		// Add a popup menu. This method set OverFlow button to visible
+		header.setButtonOverflowVisible(true);
+		header.setPopupMenuListener(new CardHeader.OnClickCardHeaderPopupMenuListener() {
+			@Override
+			public void onMenuItemClick(BaseCard card, MenuItem item) {
+				Toast.makeText(
+						context,
+						"Click on " + item.getTitle() + "-"
+								+ ((Card) card).getCardHeader().getTitle()
+								+ ", id: " + id, Toast.LENGTH_SHORT).show();
+			}
+		});
+		// Add a PopupMenuPrepareListener to add dynamically a menu entry it is
+		// optional.
+		header.setPopupMenuPrepareListener(new CardHeader.OnPrepareCardHeaderPopupMenuListener() {
+			@Override
+			public boolean onPreparePopupMenu(BaseCard card, PopupMenu popupMenu) {
+				popupMenu.getMenu().add("Share");
+				return true;
+			}
+		});
+		addCardHeader(header);
+
+		cardExpand = new CardExpand(context);
+		addCardExpand(cardExpand);
+		
+		this.setOnClickListener(new OnCardClickListener() {			
+			@Override
+			public void onClick(Card card, View view) {
+				Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+				context.startActivity(browserIntent);				
+			}
+		});
+
+	}	
+
+	private void initData() {
+		try {
+			id = bookmark.getString(StringConstants.ID);
+			url = bookmark.getString(StringConstants.URL);
+			title = bookmark.getString(StringConstants.TITLE);
+			description = bookmark.getString(StringConstants.DESCRIPTION);
+			long epoch = bookmark.getLong(StringConstants.UPDATED_AT);
+
+			Date date = new Date(epoch * 1000);
+			java.text.DateFormat format = new SimpleDateFormat(
+					"dd/MM/yyyy HH:mm:ss", Locale.US);
+			formattedDate = format.format(date);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void setData() {
+		// Set the header title
+		header.setTitle(title);
+		cardExpand.setTitle(description);
+		mDomain.setText(DomainExtractor.getBaseDomain(url));
+		mUpdatedTime.setText(formattedDate);
+		setExpanded(false);
+	}
+
+	@Override
+	public void setupInnerViewElements(ViewGroup parent, View view) {
+		// Retrieve elements
+		mDomain = (TextView) parent.findViewById(R.id.bookmark_domain);
+		mUpdatedTime = (TextView) parent
+				.findViewById(R.id.bookmark_updated_time);
+		mMore = (TextView) parent.findViewById(R.id.bookmark_more);
+		initData();
+		setData();
+		ViewToClickToExpand viewToClickToExpand = ViewToClickToExpand.builder()
+				.setupView(mMore);
+		setViewToClickToExpand(viewToClickToExpand);
+	}
+}
--- mobile_app/android/links/src/org/iiitb/se/links/home/fragments/adapter/BookmarksAdapter.java
+++ mobile_app/android/links/src/org/iiitb/se/links/home/fragments/adapter/BookmarksAdapter.java
@@ -1,30 +1,17 @@
 package org.iiitb.se.links.home.fragments.adapter;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
-
 import it.gmariotti.cardslib.library.internal.Card;
-import it.gmariotti.cardslib.library.internal.CardExpand;
-import it.gmariotti.cardslib.library.internal.CardHeader;
-import it.gmariotti.cardslib.library.internal.ViewToClickToExpand;
-import it.gmariotti.cardslib.library.internal.base.BaseCard;
 import it.gmariotti.cardslib.library.view.CardView;
 
-import org.iiitb.se.links.utils.DomainExtractor;
-import org.iiitb.se.links.utils.StringConstants;
+import org.iiitb.se.links.home.cards.BookmarkCard;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.Context;
-import android.text.format.DateFormat;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.PopupMenu;
-import android.widget.Toast;
 
 public class BookmarksAdapter extends BaseAdapter {
 
@@ -66,91 +53,26 @@ public class BookmarksAdapter extends BaseAdapter {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		JSONObject bookmark;
-		String id = null;
-		String url = null;
-		String title = null;
-		String description = null;		
-		String formattedDate = null;
-		CardView cardView = null;
-		CardHeader header = null;
-		CardExpand cardExpand = null;
-		Card card = null;
-
+		JSONObject bookmark = null;
 		try {
 			bookmark = bookmarks.getJSONObject(position);
-			id = bookmark.getString(StringConstants.ID);
-			url = bookmark.getString(StringConstants.URL);
-			title = bookmark.getString(StringConstants.TITLE);
-			description = bookmark.getString(StringConstants.DESCRIPTION);
-			long epoch = bookmark.getLong(StringConstants.UPDATED_AT);
-			
-			Date date = new Date(epoch * 1000);
-	        java.text.DateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");	        
-	        formattedDate = format.format(date);
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
+		CardView cardView = null;		
+		BookmarkCard card = null;
+		
 		if (null != convertView) {
 			cardView = (CardView) convertView;
-			card = cardView.getCard();
-			card.setTitle(DomainExtractor.getBaseDomain(url)  + " " + formattedDate);
-			header = card.getCardHeader();
-			header.setTitle(title);
-			cardExpand = card.getCardExpand();
-			cardExpand.setTitle(description);
-			cardView.refreshCard(card);
+			card = (BookmarkCard) cardView.getCard();
+			card.setBookmark(bookmark);						
 		} else {
-
-			// Create a Card
-			card = new Card(context);
-			card.setTitle(DomainExtractor.getBaseDomain(url)  + " " + formattedDate);
-
-			// Create a CardHeader
-			header = new CardHeader(context);
-
-			// Set the header title
-			header.setTitle(title);
-
-			// Add a popup menu. This method set OverFlow button to visible
-			header.setButtonOverflowVisible(true);
-			
-			header.setPopupMenuListener(new CardHeader.OnClickCardHeaderPopupMenuListener() {
-				@Override
-				public void onMenuItemClick(BaseCard card, MenuItem item) {
-					Toast.makeText(
-							context,
-							"Click on " + item.getTitle() + "-"
-									+ ((Card) card).getCardHeader().getTitle(),
-							Toast.LENGTH_SHORT).show();
-				}
-			});
-
-			// Add a PopupMenuPrepareListener to add dynamically a menu
-			// entry
-			// it is optional.
-			header.setPopupMenuPrepareListener(new CardHeader.OnPrepareCardHeaderPopupMenuListener() {
-				@Override
-				public boolean onPreparePopupMenu(BaseCard card,
-						PopupMenu popupMenu) {
-					popupMenu.getMenu().add("Share");
-					return true;
-				}
-			});
-			card.addCardHeader(header);
-
-			cardExpand = new CardExpand(context);
-			cardExpand.setTitle(description);
-			card.addCardExpand(cardExpand);
-			// Set card in the cardView
+			card = new BookmarkCard(context, bookmark);			
 			cardView = new CardView(context);
-
-			ViewToClickToExpand viewToClickToExpand = ViewToClickToExpand
-					.builder().setupView(cardView);
-			card.setViewToClickToExpand(viewToClickToExpand);
-
-			cardView.setCard(card);
+			cardView.setCard(card);			
 		}
+		
+		cardView.refreshCard(card);
 		return cardView;
 	}
 
