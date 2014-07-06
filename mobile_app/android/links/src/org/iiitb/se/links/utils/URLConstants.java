9c0ca2f178b9136becc1d696faaa8317c14c7a80
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2014-07-06 19:42:08 +0530

    Making progress dialog non-cancellable.

--- mobile_app/android/links/src/org/iiitb/se/links/utils/URLConstants.java
+++ mobile_app/android/links/src/org/iiitb/se/links/utils/URLConstants.java
@@ -1,7 +1,7 @@
 package org.iiitb.se.links.utils;
 
 public class URLConstants {
-  public static final String BASE_URL = "http://links.t.proxylocal.com";
+  public static final String BASE_URL = "http://192.168.1.13:3000";
   public static final String CALLBACK_URL = BASE_URL + "/oauth/authorize/";
   public static final String TIMELINE = BASE_URL + "/api/timeline";
   public static final String LOAD_MORE_BOOKMARKS = BASE_URL + "/api/loadmore";
