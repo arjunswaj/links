45f96f0ef577cdb254db3e028132647eb7f24d7e
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2014-07-12 10:33:28 +0530

    Removing database specific query.

--- links/links/app/helpers/bookmarks_helper.rb
+++ links/links/app/helpers/bookmarks_helper.rb
@@ -3,7 +3,7 @@ module BookmarksHelper
 	    @bookmark_plugins = PLUGIN_CONFIG['bookmark']
 	    @bookmarks = Bookmark.eager_load(:tags, :user, :url)
 	      .eager_load(group: :memberships)
-	      .where("(users.id = :user_id AND bookmarks.group_id IS NULL) OR (bookmarks.group_id IS NOT NULL AND memberships.user_id = :user_id AND memberships.acceptance_status = :membership_status)", user_id: user_id, membership_status: "t")
+	      .where("(users.id = :user_id AND bookmarks.group_id IS NULL) OR (bookmarks.group_id IS NOT NULL AND memberships.user_id = :user_id AND memberships.acceptance_status = :membership_status)", user_id: user_id, membership_status: true)
 	      .where("bookmarks.updated_at < :now", now: time)
 	      .order('bookmarks.updated_at DESC')
 	      .limit(20)
--- links/links/app/helpers/searches_helper.rb
+++ links/links/app/helpers/searches_helper.rb
@@ -3,7 +3,7 @@ module SearchesHelper
     tagname = keyword[1, keyword.length].strip.gsub(' ', '-').downcase
       @bookmarks = Bookmark.eager_load(:tags, :user, :url)
       .eager_load(group: :memberships)
-      .where("(users.id = :user_id AND bookmarks.group_id IS NULL) OR (bookmarks.group_id IS NOT NULL AND memberships.user_id = :user_id AND memberships.acceptance_status = :membership_status)", user_id: user_id, membership_status: "t")    
+      .where("(users.id = :user_id AND bookmarks.group_id IS NULL) OR (bookmarks.group_id IS NOT NULL AND memberships.user_id = :user_id AND memberships.acceptance_status = :membership_status)", user_id: user_id, membership_status: true)    
       .where("bookmarks.updated_at < :now", now: time)
       .where("LOWER(tags.tagname) = LOWER(:tag)", tag: "#{tagname}")
       .order('bookmarks.updated_at DESC')    
@@ -13,7 +13,7 @@ module SearchesHelper
   def bookmarks_searcher(time, keyword, user_id)
     @bookmarks = Bookmark.eager_load(:tags, :user, :url)
       .eager_load(group: :memberships)
-      .where("(users.id = :user_id AND bookmarks.group_id IS NULL) OR (bookmarks.group_id IS NOT NULL AND memberships.user_id = :user_id AND memberships.acceptance_status = :membership_status)", user_id: user_id, membership_status: "t")
+      .where("(users.id = :user_id AND bookmarks.group_id IS NULL) OR (bookmarks.group_id IS NOT NULL AND memberships.user_id = :user_id AND memberships.acceptance_status = :membership_status)", user_id: user_id, membership_status: true)
       .where("bookmarks.updated_at < :now", now: time)
       .where("LOWER(bookmarks.title) LIKE LOWER(:query) OR LOWER(bookmarks.description) LIKE LOWER(:query)", query: "%#{keyword}%")      
       .order('bookmarks.updated_at DESC') 
