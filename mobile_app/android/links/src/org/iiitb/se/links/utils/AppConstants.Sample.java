0db64b075300382bde25e6d233e3a4e9f838dbc6
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2014-09-16 11:28:56 +0530

    Adding sample file for AppConstants for App.

---
+++ mobile_app/android/links/src/org/iiitb/se/links/utils/AppConstants.Sample.java
@@ -0,0 +1,31 @@
+package org.iiitb.se.links.utils;
+
+/** 
+  *  Rename this to AppConstants.java 
+  * and change the API_KEY and API_SECRET
+*/
+public class AppConstants {
+  public static final String API_KEY = "YOUR_API_KEY";
+  public static final String API_SECRET = "YOUR_API_SECRET";
+
+  public static final String URN_IETF_WG_OAUTH_2_0_OOB = "urn:ietf:wg:oauth:2.0:oob";
+  
+  public static final String ACCESS_TOKEN_KEY = "ACCESS_TOKEN_KEY";
+  public static final String ACCESS_TOKEN_SECRET = "ACCESS_TOKEN_SECRET";
+  
+  public static final String FIRST_BOOKMARK_UPDATED_AT = "FIRST_BOOKMARK_UPDATED_AT";
+  public static final String LAST_BOOKMARK_UPDATED_AT = "LAST_BOOKMARK_UPDATED_AT";
+  
+  public static final String GROUPS_FIRST_BOOKMARK_UPDATED_AT = "GROUPS_FIRST_BOOKMARK_UPDATED_AT";
+  public static final String GROUPS_LAST_BOOKMARK_UPDATED_AT = "GROUPS_LAST_BOOKMARK_UPDATED_AT";
+  
+  public static final String FIRST_SEARCH_BOOKMARK_UPDATED_AT = "FIRST_SEARCH_BOOKMARK_UPDATED_AT";
+  public static final String LAST_SEARCH_BOOKMARK_UPDATED_AT = "LAST_SEARCH_BOOKMARK_UPDATED_AT";
+  
+  public static final String GROUPS_FIRST_SEARCH_BOOKMARK_UPDATED_AT = "GROUPS_FIRST_SEARCH_BOOKMARK_UPDATED_AT";
+  public static final String GROUPS_LAST_SEARCH_BOOKMARK_UPDATED_AT = "GROUPS_LAST_SEARCH_BOOKMARK_UPDATED_AT";
+  
+  public static final String LINK_FRAGMENT_OPTION_NUMBER = "link_fragment_option_number";
+  
+  public static final String SEARCH_QUERY = "SEARCH_QUERY";
+}
